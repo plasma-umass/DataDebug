fed735e59f90a67df7a910648a44fac555cdbc34
Dimitar Gochev <gochev@umass>
2013-03-05 14:17:07 -0500

    Broke up the constructTree() method

--- Ribbon.cs
+++ Ribbon.cs
@@ -27,7 +27,6 @@ namespace DataDebug
         bool[][][][] reachable_grid; //This is a multi-dimensional array of bools that will indicate whether a certain output is reachable from a certain cell
         double[][] min_max_delta_outputs; //This keeps the min and max delta for each output; first index indicates the output index; second index 0 is the min delta, 1 is the max delta for that output
         List<TreeNode> ranges;  //This is a list of all the ranges we have identified
-        List<TreeNode> charts;  //This is a list of all the charts in the workbook
         List<StartValue> starting_outputs; //This will store the values of all the output nodes at the start of the procedure for swapping values (fuzzing)
         List<TreeNode> output_cells; //This will store all the output nodes at the start of the fuzzing procedure
         List<double[]>[] reachable_impacts_grid;  //This will store impacts for cells reachable from a particular output
@@ -39,6 +38,12 @@ namespace DataDebug
         System.Diagnostics.Stopwatch global_stopwatch = new System.Diagnostics.Stopwatch();
         string stats_text = "";
         ProgBar pb;
+        TreeDict nodes;
+        TimeSpan tree_building_timespan;
+        TimeSpan impact_scoring_timespan;
+        TimeSpan swapping_timespan;
+        int outliers_count;
+        int[][][] times_perturbed;
 
         private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
         {
@@ -58,8 +63,8 @@ namespace DataDebug
         private void constructTree()
         {
             pb.SetProgress(0);
-            TimeSpan impact_scoring_timespan = global_stopwatch.Elapsed;
-            TimeSpan swapping_timespan = global_stopwatch.Elapsed;
+            impact_scoring_timespan = global_stopwatch.Elapsed;
+            swapping_timespan = global_stopwatch.Elapsed;
             input_cells_in_computation_count = 0;
             raw_input_cells_in_computation_count = 0;
 
@@ -72,12 +77,10 @@ namespace DataDebug
             //  1st: worksheet index
             //  2nd: row
             //  3rd: col
-            TreeDict nodes = ConstructTree.CreateFormulaNodes(formulaRanges, Globals.ThisAddIn.Application);
+            nodes = ConstructTree.CreateFormulaNodes(formulaRanges, Globals.ThisAddIn.Application);
             
             //This is the list of all ranges referenced in formulas
             List<Excel.Range> referencedRangesList = new List<Excel.Range>();
-            //This is the list of TreeNodes for all ranges referenced in formulas
-            List<TreeNode> referencedRangesNodeList = new List<TreeNode>();
             
             int formulaNodesCount = nodes.Count;
             //Now we parse the formulas in nodes to extract any range and cell references
@@ -91,7 +94,7 @@ namespace DataDebug
                 {
                     TreeNode rangeNode = null;
                     //See if there is an existing node for this range already in referencedRangesNodeList; if there is, do not add it again - just grab the existing one
-                    foreach (TreeNode existingNode in referencedRangesNodeList)
+                    foreach (TreeNode existingNode in ranges)
                     {
                         if (existingNode.getName().Equals(range.Address))
                         {
@@ -104,7 +107,6 @@ namespace DataDebug
                         //TODO CORRECT THE WORKBOOK PARAMETER IN THIS LINE: (IT SHOULD BE THE WORKBOOK OF range, WHICH SHOULD COME FROM GetReferencesFromFormula
                         rangeNode = new TreeNode(range.Address, range.Worksheet, node.getWorkbookObject());
                         referencedRangesList.Add(range);
-                        referencedRangesNodeList.Add(rangeNode);
                         ranges.Add(rangeNode);
                     }
 
@@ -150,109 +152,117 @@ namespace DataDebug
             ConstructTree.StoreOutputs(starting_outputs, output_cells, nodes);
             
             //Tree building stopwatch
-            TimeSpan tree_building_timespan = global_stopwatch.Elapsed;
+            tree_building_timespan = global_stopwatch.Elapsed;
+        }
+
+        private void perturbationAnalysis()
+        {
             pb.SetProgress(25);
             //Disable screen updating during perturbation to speed things up
             Globals.ThisAddIn.Application.ScreenUpdating = false;
 
             //Grids for storing influences
             double[][][] influences_grid = null;
-            int[][][] times_perturbed = null;
+            times_perturbed = null;
             //influences_grid and times_perturbed are passed by reference so that they can be modified in the setUpGrids method
             ConstructTree.setUpGrids(ref influences_grid, ref times_perturbed, Globals.ThisAddIn.Application.Worksheets, Globals.ThisAddIn.Application.Charts);
             
-            int outliers_count = 0; 
+            outliers_count = 0; 
             //Procedure for swapping values within ranges, one cell at a time
-            if (!checkBox2.Checked) //Checks if the option for swapping values simultaneously is checked (not checked by default)
-            {
-                List<TreeNode> swap_domain;
-                swap_domain = ranges;
+            //if (!checkBox2.Checked) //Checks if the option for swapping values simultaneously is checked (not checked by default)
+            //{
+            List<TreeNode> swap_domain;
+            swap_domain = ranges;
                 
-                //Initialize min_max_delta_outputs
-                min_max_delta_outputs = new double[output_cells.Count][];
-                for (int i = 0; i < output_cells.Count; i++)
-                {
-                    min_max_delta_outputs[i] = new double[2];
-                    min_max_delta_outputs[i][0] = -1.0;
-                    min_max_delta_outputs[i][1] = 0.0;
-                }
+            //Initialize min_max_delta_outputs
+            min_max_delta_outputs = new double[output_cells.Count][];
+            for (int i = 0; i < output_cells.Count; i++)
+            {
+                min_max_delta_outputs[i] = new double[2];
+                min_max_delta_outputs[i][0] = -1.0;
+                min_max_delta_outputs[i][1] = 0.0;
+            }
 
-                //Initialize impacts_grid 
-                //Initialize reachable_grid
-                impacts_grid = new double[Globals.ThisAddIn.Application.Worksheets.Count][][][];
-                reachable_grid = new bool[Globals.ThisAddIn.Application.Worksheets.Count][][][];
-                foreach (Excel.Worksheet worksheet in Globals.ThisAddIn.Application.Worksheets)
+            //Initialize impacts_grid 
+            //Initialize reachable_grid
+            impacts_grid = new double[Globals.ThisAddIn.Application.Worksheets.Count][][][];
+            reachable_grid = new bool[Globals.ThisAddIn.Application.Worksheets.Count][][][];
+            foreach (Excel.Worksheet worksheet in Globals.ThisAddIn.Application.Worksheets)
+            {
+                impacts_grid[worksheet.Index - 1] = new double[worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row][][];
+                reachable_grid[worksheet.Index - 1] = new bool[worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row][][];
+                for (int row = 0; row < (worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row); row++)
                 {
-                    impacts_grid[worksheet.Index - 1] = new double[worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row][][];
-                    reachable_grid[worksheet.Index - 1] = new bool[worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row][][];
-                    for (int row = 0; row < (worksheet.UsedRange.Rows.Count + worksheet.UsedRange.Row); row++)
+                    impacts_grid[worksheet.Index - 1][row] = new double[worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column][];
+                    reachable_grid[worksheet.Index - 1][row] = new bool[worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column][];
+                    for (int col = 0; col < (worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column); col++)
                     {
-                        impacts_grid[worksheet.Index - 1][row] = new double[worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column][];
-                        reachable_grid[worksheet.Index - 1][row] = new bool[worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column][];
-                        for (int col = 0; col < (worksheet.UsedRange.Columns.Count + worksheet.UsedRange.Column); col++)
+                        impacts_grid[worksheet.Index - 1][row][col] = new double[output_cells.Count];
+                        reachable_grid[worksheet.Index - 1][row][col] = new bool[output_cells.Count];
+                        for (int i = 0; i < output_cells.Count; i++)
                         {
-                            impacts_grid[worksheet.Index - 1][row][col] = new double[output_cells.Count];
-                            reachable_grid[worksheet.Index - 1][row][col] = new bool[output_cells.Count];
-                            for (int i = 0; i < output_cells.Count; i++)
-                            {
-                                impacts_grid[worksheet.Index - 1][row][col][i] = 0.0;
-                                reachable_grid[worksheet.Index - 1][row][col][i] = false;
-                            }
+                            impacts_grid[worksheet.Index - 1][row][col][i] = 0.0;
+                            reachable_grid[worksheet.Index - 1][row][col][i] = false;
                         }
                     }
                 }
-                
-                //Initialize reachable_impacts_grid
-                reachable_impacts_grid = new List<double[]>[output_cells.Count];
-                for (int i = 0; i < output_cells.Count; i++)
-                {
-                    reachable_impacts_grid[i] = new List<double[]>();
-                }
-
-                //Propagate weights  -- find the weights of all outputs and set up the reachable_grid entries
-                foreach (TreeDictPair tdp in nodes)
-                {
-                    var node = tdp.Value;
-                    if (!node.hasParents())
-                    {
-                        node.setWeight(1.0);  //Set the weight of all input nodes to 1.0 to start
-                        //Now we propagate it's weight to all of it's children
-                        TreeNode.propagateWeightUp(node, 1.0, node, output_cells, reachable_grid, reachable_impacts_grid);
-                        raw_input_cells_in_computation_count++;
-                    }
-                }
-
-                //Convert reachable_impacts_grid to array form
-                reachable_impacts_grid_array = new double[output_cells.Count][][];
-                for (int i = 0; i < output_cells.Count; i++)
-                {
-                    reachable_impacts_grid_array[i] = reachable_impacts_grid[i].ToArray();
-                }
-                pb.SetProgress(40);
-                ConstructTree.SwappingProcedure(swap_domain, ref input_cells_in_computation_count, ref min_max_delta_outputs, ref impacts_grid, ref times_perturbed, ref output_cells, ref reachable_grid, ref starting_outputs, ref reachable_impacts_grid_array);
-                //string text = "REACHABLE IMPACTS GRID ARRAY: \n";
-                //foreach (double[][] outputReachableList in reachable_impacts_grid_array)
-                //{
-                //    foreach (double[] input in outputReachableList)
-                //    {
-                //        text += " " + input[3];
-                //    }
-                //    text += "\n";
-                //}
-
-                //Display dd = new Display();
-                //dd.textBox1.Text = text;
-                //dd.ShowDialog();
-
-                //Stop timing swapping procedure:
-                swapping_timespan = global_stopwatch.Elapsed;
-                pb.SetProgress(80);
-                ConstructTree.ComputeZScoresAndFindOutliers(output_cells, reachable_impacts_grid_array, impacts_grid, times_perturbed, Globals.ThisAddIn.Application.Worksheets, outliers_count);
-                //Stop timing the zscore computation and outlier finding
-                pb.SetProgress(pb.progressBar1.Maximum);
-                pb.Close();
-                impact_scoring_timespan = global_stopwatch.Elapsed;
             }
+                
+            //Initialize reachable_impacts_grid
+            reachable_impacts_grid = new List<double[]>[output_cells.Count];
+            for (int i = 0; i < output_cells.Count; i++)
+            {
+                reachable_impacts_grid[i] = new List<double[]>();
+            }
+
+            //Propagate weights  -- find the weights of all outputs and set up the reachable_grid entries
+            foreach (TreeDictPair tdp in nodes)
+            {
+                var node = tdp.Value;
+                if (!node.hasParents())
+                {
+                    node.setWeight(1.0);  //Set the weight of all input nodes to 1.0 to start
+                    //Now we propagate it's weight to all of it's children
+                    TreeNode.propagateWeightUp(node, 1.0, node, output_cells, reachable_grid, reachable_impacts_grid);
+                    raw_input_cells_in_computation_count++;
+                }
+            }
+
+            //Convert reachable_impacts_grid to array form
+            reachable_impacts_grid_array = new double[output_cells.Count][][];
+            for (int i = 0; i < output_cells.Count; i++)
+            {
+                reachable_impacts_grid_array[i] = reachable_impacts_grid[i].ToArray();
+            }
+            pb.SetProgress(40);
+            ConstructTree.SwappingProcedure(swap_domain, ref input_cells_in_computation_count, ref min_max_delta_outputs, ref impacts_grid, ref times_perturbed, ref output_cells, ref reachable_grid, ref starting_outputs, ref reachable_impacts_grid_array);
+            //string text = "REACHABLE IMPACTS GRID ARRAY: \n";
+            //foreach (double[][] outputReachableList in reachable_impacts_grid_array)
+            //{
+            //    foreach (double[] input in outputReachableList)
+            //    {
+            //        text += " " + input[3];
+            //    }
+            //    text += "\n";
+            //}
+
+            //Display dd = new Display();
+            //dd.textBox1.Text = text;
+            //dd.ShowDialog();
+
+            //Stop timing swapping procedure:
+            swapping_timespan = global_stopwatch.Elapsed;
+            pb.SetProgress(80);
+        }
+
+        private void outlierAnalysis()
+        {
+            ConstructTree.ComputeZScoresAndFindOutliers(output_cells, reachable_impacts_grid_array, impacts_grid, times_perturbed, Globals.ThisAddIn.Application.Worksheets, outliers_count);
+            //Stop timing the zscore computation and outlier finding
+            pb.SetProgress(pb.progressBar1.Maximum);
+            pb.Close();
+            impact_scoring_timespan = global_stopwatch.Elapsed;
+            //}
 
             Globals.ThisAddIn.Application.ScreenUpdating = true;
             
@@ -284,7 +294,8 @@ namespace DataDebug
                 + outliers_count;
             timeDisplay.textBox1.Text = stats_text;
             timeDisplay.ShowDialog();
-
+        }
+            /*
             //Procedure for swapping values within ranges, replacing all repeated values at once
             if (checkBox2.Checked) //Checks if the option for swapping values simultaneously is checked
             {
@@ -409,7 +420,7 @@ namespace DataDebug
                     }
                 }
             }
-        }
+            */
         
         //Action for "Analyze Worksheet" button
         private void button1_Click(object sender, RibbonControlEventArgs e)
@@ -421,7 +432,6 @@ namespace DataDebug
             //Construct a new tree every time the tool is run
             nodelist = new List<TreeNode>();        //This is a list holding all the TreeNodes in the Excel file
             ranges = new List<TreeNode>();        //This is a list holding all the ranges of TreeNodes in the Excel file
-            charts = new List<TreeNode>();        //This is a list holding all the chart TreeNodes in the Excel file
             
             //Compile regular expressions
             if (toggle_compile_regex.Checked)
@@ -459,7 +469,9 @@ namespace DataDebug
                 }
             }
             pb = new ProgBar(0, 100);
-            constructTree();    
+            constructTree();
+            perturbationAnalysis();
+            outlierAnalysis();
         }
 
         //Button for testing random code :)
