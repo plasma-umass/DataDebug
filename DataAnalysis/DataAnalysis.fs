e1e97db91bf1f47e5de3fd11c8b5a3539cd37a95
Dan Barowy <dbarowy@cs.umass.edu>
2013-04-01 17:51:00 -0400

    SQL wrapper

---
+++ DataAnalysis/DataAnalysis.fs
@@ -0,0 +1,28 @@
+﻿module DataAnalysis
+
+open System.Data.SQLite
+
+type MTurkData(filename: string) =
+    do
+        let conn = new SQLiteConnection(filename)
+        conn.Open()
+
+        let cmd = new SQLiteCommand(conn)
+        cmd.CommandText <- "CREATE TABLE test (foo INTEGER)"
+        
+        cmd.ExecuteNonQuery() |> ignore
+
+//        SQLiteCommand cmd = new SQLiteCommand(conn);
+//        cmd.CommandText = "select * from Customer";
+//
+//        SQLiteDataReader reader = cmd.ExecuteReader( );
+//        while (reader.Read( ))
+//        {
+//                    // do something
+//        }
+//        reader.Close( );
+//
+//        cmd.CommandText = "delete from Customer where CustomerID = 33";
+//        cmd.ExecuteScalar( );
+//
+//        conn.Close( );
---
+++ DataAnalysis/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open DataAnalysis
+
+printfn "creating database"
+let db = DataAnalysis.MTurkData("data source=\"C:\Users\Dan Barowy\Desktop\TestDB.sql\"")
+printfn "done"
+
+match Console.ReadLine() with
+    | "quit" -> ()
+    | expr -> ()
