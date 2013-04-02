f45c80a07a94ff7501d300fff3e7982409fbef16
Dan Barowy <dbarowy@cs.umass.edu>
2013-04-02 17:20:15 -0400

    SQLite wrapper done.

--- DataAnalysis/DataAnalysis.fs
+++ DataAnalysis/DataAnalysis.fs
@@ -16,22 +16,23 @@ type MTurkData(filename: string) =
     let ToTimestamp(dt_utc: DateTime) : string =
         (dt_utc - (new DateTime(1970, 1, 1))).TotalSeconds.ToString()
     let FromMTurkTime(datestring: string) : DateTime =
-        let formatstring = "ddd MMM dd HH:mm:ss 'PDT' yyyy"
-        let dt = DateTime.ParseExact(datestring, formatstring, CultureInfo.InvariantCulture)
-        let pdt = TimeZoneInfo.CreateCustomTimeZone("Pacific Daylight Time", new TimeSpan(-07, 00, 00), "(UTC-07:00) Pacific Daylight Time", "Pacific Daylight Time")
         let utc = TimeZoneInfo.Utc
-        TimeZoneInfo.ConvertTime(dt, pdt, utc)
+        try
+            let pdtformatstring = "ddd MMM dd HH:mm:ss 'PDT' yyyy"
+            let dt = DateTime.ParseExact(datestring, pdtformatstring, CultureInfo.InvariantCulture)
+            let pdt = TimeZoneInfo.CreateCustomTimeZone("Pacific Daylight Time", new TimeSpan(-07, 00, 00), "(UTC-07:00) Pacific Daylight Time", "Pacific Daylight Time")
+            TimeZoneInfo.ConvertTime(dt, pdt, utc)
+        with
+        | _ ->
+            let gmtformatstring = "ddd MMM dd HH:mm:ss 'GMT' yyyy"
+            DateTime.ParseExact(datestring, gmtformatstring, CultureInfo.InvariantCulture)
     let TurkTimeToTimestamp(datestring: string) =
         ToTimestamp(FromMTurkTime(datestring))
-    let Connected =
+    let Connected() =
         match _conn with
         | Some(c) -> true
         | None -> false
-    override self.Finalize() =
-        match _conn with
-        | Some(c) -> c.Close()
-        | None -> ()
-    member self.CreateDatabase(dbfilename: string) =
+    let CreateDatabase() =
         let conn = new SQLiteConnection("data source=\"" + filename + "\"")
         if (conn <> null) then
             _conn <- Some(conn)
@@ -51,6 +52,7 @@ type MTurkData(filename: string) =
                                              " fileid INTEGER," +
                                              " hitid TEXT," +
                                              " hittypeid TEXT," +
+                                             " title TEXT," +
                                              " description TEXT," +
                                              " keywords TEXT," +
                                              " reward NUMERIC," +
@@ -101,48 +103,53 @@ type MTurkData(filename: string) =
 
         // populate error table
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.DigitTransposition).ToString() + "," +
-                            ErrorType.DigitTransposition.ToString() + ")"
+                            (int ErrorType.DigitTransposition).ToString() + ",\"" +
+                            ErrorType.DigitTransposition.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.ExtraDigit).ToString() + "," +
-                            ErrorType.ExtraDigit.ToString() + ")"
+                            (int ErrorType.ExtraDigit).ToString() + ",\"" +
+                            ErrorType.ExtraDigit.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.OmittedDigit).ToString() + "," +
-                            ErrorType.OmittedDigit.ToString() + ")"
+                            (int ErrorType.OmittedDigit).ToString() + ",\"" +
+                            ErrorType.OmittedDigit.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.SignError).ToString() + "," +
-                            ErrorType.SignError.ToString() + ")"
+                            (int ErrorType.SignError).ToString() + ",\"" +
+                            ErrorType.SignError.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.MantissaError).ToString() + "," +
-                            ErrorType.MantissaError.ToString() + ")"
+                            (int ErrorType.MantissaError).ToString() + ",\"" +
+                            ErrorType.MantissaError.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
         cmd.CommandText <- "INSERT INTO errortypes (id, name) VALUES (" +
-                            (int ErrorType.Other).ToString() + "," +
-                            ErrorType.Other.ToString() + ")"
+                            (int ErrorType.Other).ToString() + ",\"" +
+                            ErrorType.Other.ToString() + "\")"
         cmd.ExecuteNonQuery() |> ignore
-    member self.OpenDatabase(dbfilename: string) =
+    let OpenDatabase() =
         let conn = new SQLiteConnection("data source=\"" + filename + "\"")
         if (conn <> null) then
+            printfn "Opening database."
             _conn <- Some(conn)
         else
             failwith "Unable to connect to database."
-    member self.Command =
+    let OpenOrCreate() =
+        if System.IO.File.Exists(filename) then
+            OpenDatabase()
+        else
+            CreateDatabase()
+    do
+        OpenOrCreate()
+    member self.Command() : SQLiteCommand =
         match _conn with
         | Some(c) -> new SQLiteCommand(c)
         | None -> failwith "Unable to connect to database."
     member self.AddFile(mturkfilename: string, benchmarkfilename: string) =
-        if Connected then
-            let cmd = self.Command
-            let querytxt = "INSERT INTO files (mturkfilename, benchmarkfilename) VALUES (" + mturkfilename + ", " + benchmarkfilename + ")"
-            cmd.CommandText <- querytxt
-            if cmd.ExecuteNonQuery() <> 1 then
-                failwith ("INSERT failed: " + querytxt)
-        else
-            failwith "Must be connected to a database."
+        let cmd = self.Command()
+        let querytxt = "INSERT INTO files (mturkfilename, benchmarkfilename) VALUES (\"" + mturkfilename + "\", \"" + benchmarkfilename + "\")"
+        cmd.CommandText <- querytxt
+        if cmd.ExecuteNonQuery() <> 1 then
+            failwith ("INSERT failed: " + querytxt)
     member self.AddHIT(hitid: string,
                        hittypeid: string,
                        title: string,
@@ -154,7 +161,7 @@ type MTurkData(filename: string) =
                        requesterannotation: string,
                        assignmentdurationinseconds: int,
                        autoapprovaldelayinseconds: int,
-                       expiration: int,
+                       expiration: string,
                        numberofsimilarhits: int,
                        lifetimeinseconds: int,
                        assignmentstatus: string,
@@ -168,36 +175,33 @@ type MTurkData(filename: string) =
                        lifetimeapprovalrate: string,
                        last30daysapprovalrate: string
                        ) : int =
-        if Connected then
-            let cmd = self.Command
-            let querystr = "INSERT INTO files ( hitid, hittypeid, title, description," +
-                                              " keywords, reward, creationtime, maxassignments," +
-                                              " requesterannotation, assignmentdurationinseconds, " +
-                                              " autoapprovaldelayinseconds, expiration, numberofsimilarhits," +
-                                              " lifetimeinseconds, assignmentstatus, accepttime, submittime," +
-                                              " autoapprovaltime, approvaltime, rejectiontime, requesterfeedback," +
-                                              " worktimeinseconds, lifetimeapprovalrate, last30daysapprovalrate )"
-            let queryval = " VALUES (" + hitid + "," + hittypeid + "," + title + "," + description + ","
-                                       + keywords + "," + reward.ToString() + "," + TurkTimeToTimestamp(creationtime) + "," + maxassignments.ToString() + ","
-                                       + requesterannotation + "," + assignmentdurationinseconds.ToString() + ","
-                                       + autoapprovaldelayinseconds.ToString() + "," + expiration.ToString() + "," + numberofsimilarhits.ToString() + ","
-                                       + lifetimeinseconds.ToString() + "," + assignmentstatus + "," + TurkTimeToTimestamp(accepttime) + "," + TurkTimeToTimestamp(submittime) + ","
-                                       + TurkTimeToTimestamp(autoapprovaltime) + "," + TurkTimeToTimestamp(approvaltime) + "," + TurkTimeToTimestamp(rejectiontime) + "," + requesterfeedback + ","
-                                       + worktimeinseconds.ToString() + "," + lifetimeapprovalrate + "," + last30daysapprovalrate
-                                       + ")"
-            cmd.CommandText <- querystr + queryval
-            if cmd.ExecuteNonQuery() <> 1 then
-                failwith ("INSERT failed: " + querystr + queryval)
+        let cmd = self.Command()
+        let querystr = "INSERT INTO hits ( hitid, hittypeid, title, description," +
+                                         " keywords, reward, creationtime, maxassignments," +
+                                         " requesterannotation, assignmentdurationinseconds, " +
+                                         " autoapprovaldelayinseconds, expiration, numberofsimilarhits," +
+                                         " lifetimeinseconds, assignmentstatus, accepttime, submittime," +
+                                         " autoapprovaltime, approvaltime, rejectiontime, requesterfeedback," +
+                                         " worktimeinseconds, lifetimeapprovalrate, last30daysapprovalrate )"
+        let queryval = " VALUES (\"" + hitid + "\",\"" + hittypeid + "\",\"" + title + "\",\"" + description + "\",\""
+                                    + keywords + "\"," + reward.ToString() + "," + TurkTimeToTimestamp(creationtime) + "," + maxassignments.ToString() + ",\""
+                                    + requesterannotation + "\"," + assignmentdurationinseconds.ToString() + ","
+                                    + autoapprovaldelayinseconds.ToString() + "," + TurkTimeToTimestamp(expiration) + "," + numberofsimilarhits.ToString() + ","
+                                    + lifetimeinseconds.ToString() + ",\"" + assignmentstatus + "\"," + TurkTimeToTimestamp(accepttime) + "," + TurkTimeToTimestamp(submittime) + ","
+                                    + TurkTimeToTimestamp(autoapprovaltime) + "," + TurkTimeToTimestamp(approvaltime) + "," + TurkTimeToTimestamp(rejectiontime) + ",\"" + requesterfeedback + "\","
+                                    + worktimeinseconds.ToString() + ",\"" + lifetimeapprovalrate + "\",\"" + last30daysapprovalrate + "\")"
+        cmd.CommandText <- querystr + queryval
+        if cmd.ExecuteNonQuery() <> 1 then
+            failwith ("INSERT failed: " + querystr + queryval)
 
-            // return the id
-            cmd.CommandText <- "SELECT LAST_INSERT_ROWID();"
-            System.Convert.ToInt32(cmd.ExecuteScalar())
-        else
-            failwith "Must be connected to a database."
+        // return the id
+        cmd.CommandText <- "SELECT LAST_INSERT_ROWID();"
+        System.Convert.ToInt32(cmd.ExecuteScalar())
     member self.AddAnswerWithErrors(cell: int, data: string, hitid: int, errors: ErrorType list) : int =
-        if Connected then
-            let cmd = self.Command
-            let querystr = "INSERT INTO answers (cell, data, hitid) VALUES (" + cell.ToString() + "," + data + "," + hitid.ToString() + ")"
+        if Connected() then
+            let cmd = self.Command()
+            let querystr = "INSERT INTO answers (cell, data, hitid) VALUES (" + cell.ToString() + ",\"" + data + "\"," + hitid.ToString() + ")"
+            cmd.CommandText <- querystr
             if cmd.ExecuteNonQuery() <> 1 then
                 failwith ("INSERT failed: " + querystr)
 
@@ -207,7 +211,7 @@ type MTurkData(filename: string) =
 
             // add all of the error classifications
             for error in errors do
-                let querystr = "INSERT INTO answers_errors (answerid, errortypeid) VALUES (" + answer_id.ToString() + "," + error.ToString() + ")"
+                let querystr = "INSERT INTO answers_errors (answerid, errortypeid) VALUES (" + answer_id.ToString() + "," + (int error).ToString() + ")"
                 cmd.CommandText <- querystr
                 if cmd.ExecuteNonQuery() <> 1 then
                     failwith ("INSERT failed: " + querystr)
--- DataAnalysis/Program.fs
+++ DataAnalysis/Program.fs
@@ -4,6 +4,34 @@ open System.IO
 
 printfn "creating database"
 let db = DataAnalysis.MTurkData("C:\Users\Dan Barowy\Desktop\TestDB.sql")
+db.AddFile("foo.xls", "blah.csv")
+let hitid = db.AddHIT("hitid",
+                      "hittypeid",
+                      "title",
+                      "description",
+                      "keywords",
+                      0.01m,
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      1,
+                      "requesterannotation",
+                      1,
+                      1,
+                      "Tue Mar 26 22:56:54 PDT 2013",
+                      1,
+                      1,
+                      "assignmentstatus",
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      "Tue Mar 26 22:56:54 GMT 2013",
+                      "requesterfeedback",
+                      1,
+                      "lifetimeapprovalrate",
+                      "last30daysapprovalrate")
+let answer1 = db.AddAnswerWithErrors(1, "blahblahblah", hitid, [DataAnalysis.ErrorType.ExtraDigit])
+let answer2 = db.AddAnswerWithErrors(2, "foofoofoo", hitid, [DataAnalysis.ErrorType.MantissaError])
+
 printfn "done"
 
 match Console.ReadLine() with
