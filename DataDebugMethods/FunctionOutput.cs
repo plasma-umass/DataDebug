1baba05a89852452f661391fa1348b51221ffc43
Dimitar Gochev <gochev@umass>
2013-05-13 16:31:40 -0400

    Minor edits.

--- DataDebugMethods/FunctionOutput.cs
+++ DataDebugMethods/FunctionOutput.cs
@@ -9,15 +9,18 @@ namespace DataDebugMethods
     {
         private T _value;
         private HashSet<int> _excludes;
+
         public FunctionOutput(T value, HashSet<int> excludes)
         {
             _value = value;
             _excludes = excludes;
         }
+
         public T GetValue()
         {
             return _value;
         }
+
         public HashSet<int> GetExcludes()
         {
             return _excludes;
