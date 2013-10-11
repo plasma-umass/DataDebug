a209d3b7c2ee58877f00f0819dc1b4a2370f9595
U-ADS\dbarowy <dbarowy@shasta.ads.cs.umass.edu>
2013-10-11 14:42:10 -0400

    new empty user simulation project

---
+++ DataDebugMethods/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace DataDebugMethods
+{
+    class Utility
+    {
+        static Excel.Workbook OpenWorkbook(string filename, Excel.Application app)
+        {
+            // we need to disable all alerts, e.g., password prompts, etc.
+            app.DisplayAlerts = false;
+
+            // disable macros
+            app.AutomationSecurity = Microsoft.Office.Core.MsoAutomationSecurity.msoAutomationSecurityForceDisable;
+
+            // This call is stupid.  See:
+            // http://msdn.microsoft.com/en-us/library/microsoft.office.interop.excel.workbooks.open%28v=office.11%29.aspx
+            app.Workbooks.Open(filename, 2, true, Missing.Value, "thisisnotapassword", Missing.Value, true, Missing.Value, Missing.Value, Missing.Value, false, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+
+            return app.Workbooks[1];
+        }
+    }
+}
---
+++ UserSimulation/Simulation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using TreeNode = DataDebugMethods.TreeNode;
+using CellDict = System.Collections.Generic.Dictionary<AST.Address, string>;
+
+namespace UserSimulation
+{
+    public class Simulation
+    {
+        CellDict saved_values = new CellDict();
+        HashSet<AST.Address> tool_highlights = new HashSet<AST.Address>();
+        HashSet<AST.Address> known_good = new HashSet<AST.Address>();
+        IEnumerable<Tuple<double, TreeNode>> analysis_results = null;
+        AST.Address flagged_cell = null;
+
+        // create and run a CheckCell simulation
+        public Simulation(string filename, double sensitivity, CellDict errors, Excel.Application app)
+        {
+            // open spreadsheet
+
+            // save original spreadsheet state
+        }
+
+        // save spreadsheet state to a CellDict
+        public CellDict SaveSpreadsheet(Excel.Workbook wb)
+        {
+        }
+    }
+}
