e78f26c83ffa3d696973b7fea34b7355cde5e39f
Dan Barowy <dbarowy@cs.umass.edu>
2013-11-02 10:22:34 -0400

    Adding (untested) LCS code.

---
+++ CheckCellTests/LCSTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckCellTests
+{
+    [TestClass]
+    public class LCSTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s1 = "Hello";
+            var s2 = "Helloo";
+            var ss = LongestCommonSubsequence.LCS_Hash(s1, s2);
+            Assert.AreEqual(true, ss.Contains(s1));
+        }
+    }
+}
---
+++ LongestCommonSubsequence/LCS.fs
@@ -0,0 +1,40 @@
+﻿module LongestCommonSubsequence
+    // compute all of the LCS lengths
+    let LCSLength(X: string, Y: string) : int[,] =
+        let m = X.Length
+        let n = Y.Length
+        let C = Array2D.create m n 0
+        for i = 1 to m - 1 do
+            for j = 1 to n - 1 do
+                if X.[i] = Y.[j] then
+                    C.[i,j] <- C.[i-1,j-1] + 1
+                else
+                    C.[i,j] <- System.Math.Max(C.[i,j-1],C.[i-1,j])
+        C
+
+    // this function reads out all of the longest subsequences
+    let rec backtrackAll(C: int[,], X: string, Y: string, i: int, j: int) : Set<string> =
+        if i = 0 || j = 0 then
+            Set.empty
+        else if X.[i] = Y.[j] then
+            let ZS = Set.map (fun (Z: string) -> Z + X.[i].ToString()) (backtrackAll(C, X, Y, i-1, j-1))
+            ZS
+        else
+            let mutable R = Set.empty
+            if C.[i,j-1] >= C.[i-1,j] then
+                R <- backtrackAll(C, X, Y, i, j-1)
+            if C.[i-1,j] >= C.[i,j-1] then
+                R <- Set.union R (backtrackAll(C, X, Y, i-1, j))
+            R
+
+    // compute the set of longest strings
+    let LCS(X: string, Y: string) : Set<string> =
+        let m = X.Length
+        let n = Y.Length
+        let C = LCSLength(X,Y)
+        backtrackAll(C, X, Y, m - 1, n - 1)
+
+    let LCS_Hash(X: string, Y: string) : System.Collections.Generic.HashSet<string> =
+        let hs = new System.Collections.Generic.HashSet<string>()
+        for s in LCS(X,Y) do hs.Add(s) |> ignore
+        hs
