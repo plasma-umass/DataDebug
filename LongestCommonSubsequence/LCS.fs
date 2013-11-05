0fe5536f688afbe46ca5fa5c4e52a073e159701b
U-ADS\dbarowy <dbarowy@shasta.ads.cs.umass.edu>
2013-11-05 16:47:06 -0500

    LCS_Hash_Char actually needs to return all possible character sequence pairings.

--- LongestCommonSubsequence/LCS.fs
+++ LongestCommonSubsequence/LCS.fs
@@ -49,7 +49,10 @@
         if i = 0 || j = 0 then
             set[[]]
         else if X.[i-1] = Y.[j-1] then
-            Set.map (fun (Z: (int*int) list) -> Z @ [(i-1,j-1)] ) (getCharPairs(C, X, Y, i-1, j-1))
+            let mutable ZS = Set.map (fun (Z: (int*int) list) -> Z @ [(i-1,j-1)] ) (getCharPairs(C, X, Y, i-1, j-1))
+            if (C.[i,j] = C.[i,j-1]) then 
+                ZS <- Set.union ZS (getCharPairs(C, X, Y, i, j-1))
+            ZS
         else
             let mutable R = Set.empty
             if C.[i,j-1] >= C.[i-1,j] then
@@ -74,17 +77,28 @@
 
     // "pull" each Y index to the left as far as it will go
     // this allows key-repeat typos to be counted correctly
-    let LCS_pull(orig: string, entered: string) : Set<(int*int) list> =
+    let LeftAlignedLCS(orig: string, entered: string) : (int*int) list =
         let charseqs = LCS_Char(orig, entered) |> Set.toArray
         // randomly choose one of the longest subsequences
         let rng = System.Random()
         let charseq = charseqs.[rng.Next(charseqs.Length - 1)]
+        // new sequence
+        let mutable newseq: (int*int) list = []
         // realign characters
-//        List.fold (fun jstop (i,j) ->
-//            candidate_indices = List.rev [jstop..j]
-//
-//        ) 0 charseq
-        failwith "foo"
+        let mutable jstop = 0
+        for (i,j) in charseq do
+            // all of the candidates to the left of j, but only as far as jstop
+            let candidate_indices = List.rev [jstop..j]
+            let mutable keep_looking = true
+            for k in candidate_indices do
+                if keep_looking then
+                    if entered.[k] = entered.[j] then
+                        jstop <- k
+                    else
+                        keep_looking <- false
+            newseq <- newseq @ [(i,jstop)]
+        // return realigned char list
+        newseq
 
     let LCS_Hash(X: string, Y: string) : System.Collections.Generic.HashSet<string> =
         let hs = new System.Collections.Generic.HashSet<string>()
