cb554bf5c623468317c8dd4957ceed98c2fb74c7
Dimitar Gochev <gochev@umass>
2013-11-05 18:13:34 -0500

    Added sign error generator.

--- UserSimulation/ErrorGenerator.cs
+++ UserSimulation/ErrorGenerator.cs
@@ -13,12 +13,52 @@ namespace UserSimulation
     {
         public static ErrorString GenerateErrorString(string input)
         {
-            ErrorString output = new Tuple<string, List<LCSError>>("",null);
+            String modified_input = input;
             //try to add a sign error
-
-            //
+            Sign s = Classification.getSign(input);
+            var kvps = _sign_dict.Where(pair => pair.Key.Item1 == s);
+            var sum = kvps.Select(pair => pair.Value).Sum();
+            var rng = new Random();
+            var bins = kvps.Select(pair => Enumerable.Repeat(pair.Key,pair.Value)).SelectMany( i => i);
+            var tpl = bins.ElementAt(rng.Next(bins.Count()));
+            Sign s2 = tpl.Item2;
+            
+            if (s != s2)
+            {
+                if (s == Sign.Empty)
+                {
+                    if (s2 == Sign.Plus)
+                    {
+                        modified_input = "+" + modified_input;
+                    }
+                    else if (s2 == Sign.Minus)
+                    {
+                        modified_input = "-" + modified_input;
+                    }
+                }
+                else
+                {
+                    if (s2 == Sign.Plus)
+                    {
+                        modified_input = "+" + modified_input.Remove(0,1);
+                    }
+                    else if (s2 == Sign.Minus)
+                    {
+                        modified_input = "-" + modified_input.Remove(0, 1);
+                    }
+                    else
+                    {
+                        modified_input = modified_input.Remove(0, 1);
+                    }
+                }
+            }
+            
+            //if there is a decimal in the input, try to add a decimal error
 
             //Adding a decimal is handled by inserted characters
+
+            ErrorString output = new Tuple<string, List<LCSError>>("", null);
+            
             return output;
         }
     }
