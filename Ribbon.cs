7c91378893333c3d549cb464fe737543d16a7fcc
Dimitar Gochev <gochev@umass>
2014-03-22 23:04:42 -0400

    ...

--- Ribbon.cs
+++ Ribbon.cs
@@ -152,41 +152,7 @@ namespace DataDebug
             return FSharpOption<double>.Some(significance);
         }
 
-        private void MyPrecioussssss()
-        {
-            //Should we be using an outlier test for 
-            //highlighting scores that fall outside of two standard deviations from the others?
-            //(The one-sided 5% cutoff for the normal distribution is 1.6448.)
-            //Or do we always want to be highlighting the top 5% of the scores?
-            //Currently, if we have something like this, we don't flag anything 
-            //because the 1 value that is weird is an entire 20% of the total:
-            //     1,1,1,1000,1     =SUM(A1:A5)
-            //-Dimitar
-
-            ////Code for doing normal outlier analysis on the scores:
-            ////find mean:
-            //double sum = 0.0;
-            //foreach (double d in scores.Values)
-            //{
-            //    sum += d;
-            //}
-            //double mean = sum / scores.Values.Count;
-            ////find variance
-            //double distance_sum_sq = 0.0;
-            //foreach (double d in scores.Values)
-            //{
-            //    distance_sum_sq += Math.Pow(mean - d, 2);
-            //}
-            //double variance = distance_sum_sq / scores.Values.Count;
-
-            ////find std. deviation
-            //double std_deviation = Math.Sqrt(variance);
-
-            //var filtered_high_scores = scores_list.Where(kvp => kvp.Value > mean + std_deviation * 1.6448).ToList();
-        }
-
-        //private IEnumerable<Tuple<double,TreeNode>> Analyze()
-        private List<KeyValuePair<TreeNode, int>> Analyze()
+        private List<KeyValuePair<TreeNode, int>> Analyze(bool normal_cutoff)
         {
             using (var pb = new ProgBar(0, 100))
             {
@@ -219,46 +185,90 @@ namespace DataDebug
                 var scores = Analysis.Bootstrap(NBOOTS, data, app, true, true);
                 var scores_list = scores.OrderByDescending(pair => pair.Value).ToList();
 
-                MyPrecioussssss();
+                List<KeyValuePair<TreeNode, int>> filtered_high_scores;
 
-                int start_ptr = 0;
-                int end_ptr = 0;
+                //Using an outlier test for highlighting 
+                //scores that fall outside of two standard deviations from the others
+                //The one-sided 5% cutoff for the normal distribution is 1.6448.
 
-                List<KeyValuePair<TreeNode, int>> high_scores = new List<KeyValuePair<TreeNode, int>>();
-
-                while ((double)start_ptr / scores_list.Count < 1.0 - tool_significance) //the start of this score region is before the cutoff
+                if (normal_cutoff)
                 {
-                    //while the scores at the start and end pointers are the same, bump the end pointer
-                    while (end_ptr < scores_list.Count && scores_list[start_ptr].Value == scores_list[end_ptr].Value)
+                    //Code for doing normal outlier analysis on the scores:
+                    //find mean:
+                    double sum = 0.0;
+                    foreach (double d in scores.Values)
                     {
-                        end_ptr++;
+                        sum += d;
                     }
-                    //Now the end_pointer points to the first index with a lower score
-                    //If the number of entries with the current value is fewer than the significance cutoff, add all values of this score to the high_scores list; the number of entries is equal to the end_ptr since end_ptr is zero-based
-                    //There is some added "wiggle room" to the cutoff, so that the last entry is allowed to straddle the cutoff bound.
-                    //  To do this, we add (1 / total number of entries) to the cutoff
-                    //The purpose of the wiggle room is to allow us to deal with small ranges (less than 20 entries), since a single entry accounts
-                    //for more than 5% of the total.
-                    //      Note: tool_significance is along the lines of 0.95 (not 0.05).
-                    if ((double)end_ptr / scores_list.Count < 1.0 - tool_significance + (double)1.0/scores_list.Count)
+                    double mean = sum / scores.Values.Count;
+                    //find variance
+                    double distance_sum_sq = 0.0;
+                    foreach (double d in scores.Values)
                     {
-                        //add all values of the current score to high_scores list
-                        for (; start_ptr < end_ptr; start_ptr++)
-                        {
-                            high_scores.Add(scores_list[start_ptr]);
-                        }
-                        //Increment the start pointer to the start of the next score region
-                        start_ptr++;
+                        distance_sum_sq += Math.Pow(mean - d, 2);
                     }
-                    else    //if this score region extends past the cutoff, we don't add any of its values to the high_scores list, and stop
+                    double variance = distance_sum_sq / scores.Values.Count;
+
+                    //find std. deviation
+                    double std_deviation = Math.Sqrt(variance);
+
+                    if (tool_significance == 0.95)
                     {
-                        break;
+                        filtered_high_scores = scores_list.Where(kvp => kvp.Value > mean + std_deviation * 1.6448).ToList();
+                    }
+                    else if (tool_significance == 0.9)   //10% cutoff 1.2815
+                    {
+                        filtered_high_scores = scores_list.Where(kvp => kvp.Value > mean + std_deviation * 1.2815).ToList();
+                    }
+                    else if (tool_significance == 0.975) //2.5% cutoff 1.9599
+                    {
+                        filtered_high_scores = scores_list.Where(kvp => kvp.Value > mean + std_deviation * 1.9599).ToList();
+                    }
+                    else if (tool_significance == 0.925) //7.5% cutoff 1.4395
+                    {
+                        filtered_high_scores = scores_list.Where(kvp => kvp.Value > mean + std_deviation * 1.4395).ToList();
                     }
                 }
+                else
+                {
+                    int start_ptr = 0;
+                    int end_ptr = 0;
 
-                // filter out cells marked as OK
-                var filtered_high_scores = high_scores.Where(kvp => !known_good.Contains(kvp.Key.GetAddress())).ToList();
-                
+                    List<KeyValuePair<TreeNode, int>> high_scores = new List<KeyValuePair<TreeNode, int>>();
+
+                    while ((double)start_ptr / scores_list.Count < 1.0 - tool_significance) //the start of this score region is before the cutoff
+                    {
+                        //while the scores at the start and end pointers are the same, bump the end pointer
+                        while (end_ptr < scores_list.Count && scores_list[start_ptr].Value == scores_list[end_ptr].Value)
+                        {
+                            end_ptr++;
+                        }
+                        //Now the end_pointer points to the first index with a lower score
+                        //If the number of entries with the current value is fewer than the significance cutoff, add all values of this score to the high_scores list; the number of entries is equal to the end_ptr since end_ptr is zero-based
+                        //There is some added "wiggle room" to the cutoff, so that the last entry is allowed to straddle the cutoff bound.
+                        //  To do this, we add (1 / total number of entries) to the cutoff
+                        //The purpose of the wiggle room is to allow us to deal with small ranges (less than 20 entries), since a single entry accounts
+                        //for more than 5% of the total.
+                        //      Note: tool_significance is along the lines of 0.95 (not 0.05).
+                        if ((double)end_ptr / scores_list.Count < 1.0 - tool_significance + (double)1.0 / scores_list.Count)
+                        {
+                            //add all values of the current score to high_scores list
+                            for (; start_ptr < end_ptr; start_ptr++)
+                            {
+                                high_scores.Add(scores_list[start_ptr]);
+                            }
+                            //Increment the start pointer to the start of the next score region
+                            start_ptr++;
+                        }
+                        else    //if this score region extends past the cutoff, we don't add any of its values to the high_scores list, and stop
+                        {
+                            break;
+                        }
+                    }
+
+                    // filter out cells marked as OK
+                    filtered_high_scores = high_scores.Where(kvp => !known_good.Contains(kvp.Key.GetAddress())).ToList();
+                }
                 // Enable screen updating when we're done
                 app.ScreenUpdating = true;
                 return filtered_high_scores;
@@ -340,7 +350,7 @@ namespace DataDebug
                 tool_significance = sig.Value;
                 try
                 {
-                    flaggable_list = Analyze();
+                    flaggable_list = Analyze(false);
                     Flag();
                 }
                 catch (ExcelParserUtility.ParseException ex)
@@ -396,7 +406,7 @@ namespace DataDebug
                 flagged_cell = null;
                 try
                 {
-                    flaggable_list = Analyze();
+                    flaggable_list = Analyze(false);
                     Flag();
                 }
                 catch (ExcelParserUtility.ParseException ex)
