5f361afacf9507d6c3794b6d3d56a70223a912a3
Daniel Barowy <dbarowy@cs.umass.edu>
2014-09-30 16:11:57 -0400

    save

--- DataDebugMethods/AddressCache.cs
+++ DataDebugMethods/AddressCache.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Excel = Microsoft.Office.Interop.Excel;
 using AddrDict = System.Collections.Generic.Dictionary<Microsoft.Office.Interop.Excel.Range, AST.Address>;
+using FormulaDict = System.Collections.Generic.Dictionary<AST.Address, string>;
 
 namespace DataDebugMethods
 {
     public class AddressCache
     {
         AddrDict _cache = new AddrDict();
+        FormulaDict _formulas = new FormulaDict();
 
         public AddressCache(Excel.Workbook wb, Excel.Application app)
         {
@@ -18,7 +21,10 @@ namespace DataDebugMethods
             var path = new Microsoft.FSharp.Core.FSharpOption<String>(wb.Path);
 
             // init R1C1 extractor
-            var regex = new System.Text.RegularExpressions.Regex("^R([0-9]+)C([0-9]+)$");
+            var regex = new Regex("^R([0-9]+)C([0-9]+)$");
+
+            // init formula validator
+            var fn_filter = new Regex("^=", RegexOptions.Compiled);
 
             foreach (Excel.Worksheet worksheet in wb.Worksheets)
             {
@@ -36,9 +42,9 @@ namespace DataDebugMethods
 
                 // init
                 int x_max = right - left;
+                int y_max = bottom - top;
                 int x = -1;
                 int y = 0;
-
 
                 // for each COM object in the used range, create an address object
                 // WITHOUT calling any methods on the COM object itself
@@ -59,11 +65,34 @@ namespace DataDebugMethods
                     var addr = AST.Address.NewFromR1C1(r, c, wsname, wbname, path);
                     _cache.Add(cell, addr);
                 }
+
+                // array read of formula cells
+                // note that this is a 1-based 2D multiarray
+                object[,] formulas = rng.Formula;
+
+                // for every cell that is actually a formula, add to 
+                // formula dictionary
+                for (int c = 1; c <= x_max; c++)
+                {
+                    for (int r = 1; r <= y_max; r++)
+                    {
+                        var f = (string)formulas[c,r];
+                        if (fn_filter.IsMatch(f)) {
+                            var addr = AST.Address.NewFromR1C1(r, c, wsname, wbname, path);
+                            _formulas.Add(addr, f);
+                        }
+                    }
+                }
             }
         }
 
         public AST.Address GetAddressOfCell(Excel.Range cell) {
             return _cache[cell];
+        }
+
+        public AST.Address[] GetFormulaCells()
+        {
+            throw new NotImplementedException();
         }
     }
 }
