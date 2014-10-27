e04bc363d00e0084cae34c942efe78e63c9e9c15
Dan Barowy <dbarowy@cs.umass.edu>
2014-10-27 16:12:06 -0400

    More.

---
+++ CheckCell/Ribbon.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CheckCell
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.CheckCellTab = this.Factory.CreateRibbonTab();
+            this.CheckCellGroup = this.Factory.CreateRibbonGroup();
+            this.box1 = this.Factory.CreateRibbonBox();
+            this.AnalyzeButton = this.Factory.CreateRibbonButton();
+            this.MarkAsOKButton = this.Factory.CreateRibbonButton();
+            this.FixErrorButton = this.Factory.CreateRibbonButton();
+            this.StartOverButton = this.Factory.CreateRibbonButton();
+            this.ToDOTButton = this.Factory.CreateRibbonButton();
+            this.SensitivityTextBox = this.Factory.CreateRibbonEditBox();
+            this.tab1.SuspendLayout();
+            this.CheckCellTab.SuspendLayout();
+            this.CheckCellGroup.SuspendLayout();
+            this.box1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // CheckCellTab
+            // 
+            this.CheckCellTab.Groups.Add(this.CheckCellGroup);
+            this.CheckCellTab.Label = "CheckCell";
+            this.CheckCellTab.Name = "CheckCellTab";
+            // 
+            // CheckCellGroup
+            // 
+            this.CheckCellGroup.Items.Add(this.box1);
+            this.CheckCellGroup.Items.Add(this.SensitivityTextBox);
+            this.CheckCellGroup.Name = "CheckCellGroup";
+            // 
+            // box1
+            // 
+            this.box1.Items.Add(this.AnalyzeButton);
+            this.box1.Items.Add(this.MarkAsOKButton);
+            this.box1.Items.Add(this.FixErrorButton);
+            this.box1.Items.Add(this.StartOverButton);
+            this.box1.Items.Add(this.ToDOTButton);
+            this.box1.Name = "box1";
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.Label = "Analyze";
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.AnalyzeButton_Click);
+            // 
+            // MarkAsOKButton
+            // 
+            this.MarkAsOKButton.Label = "Mark as OK";
+            this.MarkAsOKButton.Name = "MarkAsOKButton";
+            // 
+            // FixErrorButton
+            // 
+            this.FixErrorButton.Label = "Fix Error";
+            this.FixErrorButton.Name = "FixErrorButton";
+            // 
+            // StartOverButton
+            // 
+            this.StartOverButton.Label = "Start Over";
+            this.StartOverButton.Name = "StartOverButton";
+            // 
+            // ToDOTButton
+            // 
+            this.ToDOTButton.Label = "To DOT";
+            this.ToDOTButton.Name = "ToDOTButton";
+            this.ToDOTButton.Visible = false;
+            // 
+            // SensitivityTextBox
+            // 
+            this.SensitivityTextBox.Label = "% Most Unusual to Show";
+            this.SensitivityTextBox.Name = "SensitivityTextBox";
+            this.SensitivityTextBox.SizeString = "100.0";
+            this.SensitivityTextBox.Text = "5.0";
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.CheckCellTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.CheckCellTab.ResumeLayout(false);
+            this.CheckCellTab.PerformLayout();
+            this.CheckCellGroup.ResumeLayout(false);
+            this.CheckCellGroup.PerformLayout();
+            this.box1.ResumeLayout(false);
+            this.box1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab CheckCellTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup CheckCellGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonBox box1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton AnalyzeButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton MarkAsOKButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton FixErrorButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton StartOverButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ToDOTButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox SensitivityTextBox;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ CheckCell/Ribbon.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using DataDebugMethods;
+using TreeScore = System.Collections.Generic.Dictionary<AST.Address, int>;
+using TreeDict = System.Collections.Generic.Dictionary<AST.Address, AST.Address>;
+using Microsoft.FSharp.Core;
+using System.IO;
+using System.Linq;
+using OptTuple = Microsoft.FSharp.Core.FSharpOption<System.Tuple<UserSimulation.Classification, string>>;
+
+namespace CheckCell
+{
+    public partial class Ribbon
+    {
+        // workbook state data
+        Dictionary<Excel.Workbook, WorkbookState> wbstates = new Dictionary<Excel.Workbook, WorkbookState>();
+        WorkbookState current_workbook;
+
+        // simulation files
+        string classification_file;
+        String benchmark_dir;
+        String simulation_output_dir;
+        String simulation_classification_file;
+
+        private void SetUIState(WorkbookState wbs)
+        {
+            this.MarkAsOKButton.Enabled = wbs.MarkAsOK_Enabled;
+            this.FixErrorButton.Enabled = wbs.FixError_Enabled;
+            this.StartOverButton.Enabled = wbs.ClearColoringButton_Enabled;
+            this.AnalyzeButton.Enabled = wbs.Analyze_Enabled;
+        }
+
+        private void SetUIStateNoWorkbooks()
+        {
+            this.MarkAsOKButton.Enabled = false;
+            this.FixErrorButton.Enabled = false;
+            this.StartOverButton.Enabled = false;
+            this.AnalyzeButton.Enabled = false;
+        }
+
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            // Callbacks for handling workbook state objects
+            //WorkbookOpen(Globals.ThisAddIn.Application.ActiveWorkbook);
+            //((Excel.AppEvents_Event)Globals.ThisAddIn.Application).NewWorkbook += WorkbookOpen;
+            Globals.ThisAddIn.Application.WorkbookOpen += WorkbookOpen;
+            Globals.ThisAddIn.Application.WorkbookActivate += WorkbookActivated;
+            Globals.ThisAddIn.Application.WorkbookDeactivate += WorkbookDeactivated;
+            Globals.ThisAddIn.Application.WorkbookBeforeClose += WorkbookClose;
+        }
+
+        // This event is called when Excel opens a workbook
+        private void WorkbookOpen(Excel.Workbook workbook)
+        {
+            wbstates.Add(workbook, new WorkbookState(Globals.ThisAddIn.Application, workbook));
+        }
+
+        // This event is called when Excel brings an opened workbook
+        // to the foreground
+        private void WorkbookActivated(Excel.Workbook workbook)
+        {
+            current_workbook = wbstates[workbook];
+            SetUIState(current_workbook);
+        }
+
+        // This even it called when Excel sends an opened workbook
+        // to the background
+        private void WorkbookDeactivated(Excel.Workbook workbook)
+        {
+            current_workbook = null;
+            // WorkbookBeforeClose event does not fire for default workbooks
+            // containing no data
+            var wbs = new List<Excel.Workbook>();
+            foreach (var wb in Globals.ThisAddIn.Application.Workbooks)
+            {
+                if (wb != workbook)
+                {
+                    wbs.Add((Excel.Workbook)wb);
+                }
+            }
+
+            if (wbs.Count == 0)
+            {
+                wbstates.Clear();
+                SetUIStateNoWorkbooks();
+            }
+        }
+
+        private void WorkbookClose(Excel.Workbook workbook, ref bool Cancel)
+        {
+            wbstates.Remove(workbook);
+            if (wbstates.Count == 0)
+            {
+                SetUIStateNoWorkbooks();
+            }
+        }
+
+        #region BUTTON_HANDLERS
+        private void AnalyzeButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            // check for debug easter egg
+            if ((System.Windows.Input.Keyboard.Modifiers & System.Windows.Input.ModifierKeys.Alt) > 0)
+            {
+                current_workbook.DebugMode = true;
+            }
+
+            var sig = GetSignificance(this.SensitivityTextBox.Text, this.SensitivityTextBox.Label);
+            if (sig == FSharpOption<double>.None)
+            {
+                return;
+            }
+            else
+            {
+                current_workbook.ToolSignificance = sig.Value;
+                try
+                {
+                    current_workbook.Analyze(WorkbookState.MAX_DURATION_IN_MS);
+                    current_workbook.Flag();
+                    SetUIState(current_workbook);
+                }
+                catch (ExcelParserUtility.ParseException ex)
+                {
+                    System.Windows.Forms.Clipboard.SetText(ex.Message);
+                    System.Windows.Forms.MessageBox.Show("Could not parse the formula string:\n" + ex.Message);
+                    return;
+                }
+                catch (System.OutOfMemoryException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show("Insufficient memory to perform analysis.");
+                    return;
+                }
+            }
+        }
+
+        private void FixErrorButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            current_workbook.FixError(SetUIState);
+        }
+
+        private void MarkAsOKButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            current_workbook.MarkAsOK();
+            SetUIState(current_workbook);
+        }
+
+        private void StartOverButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            current_workbook.ResetTool();
+            SetUIState(current_workbook);
+        }
+
+        private void ToDOTButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            var app = Globals.ThisAddIn.Application;
+            var dag = new DAG(app.ActiveWorkbook, app, true);
+            System.Windows.Forms.Clipboard.SetText(dag.ToDOT());
+            System.Windows.Forms.MessageBox.Show("In clipboard");
+        }
+        #endregion BUTTON_HANDLERS
+
+        #region UTILITY_FUNCTIONS
+        private static FSharpOption<double> GetSignificance(string input, string label)
+        {
+            var errormsg = label + " must be a value between 0 and 100";
+            var significance = 0.95;
+
+            try
+            {
+                significance = (100.0 - Double.Parse(input)) / 100.0;
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show(errormsg);
+            }
+
+            if (significance < 0 || significance > 100)
+            {
+                System.Windows.Forms.MessageBox.Show(errormsg);
+            }
+
+            return FSharpOption<double>.Some(significance);
+        }
+        #endregion UTILITY_FUNCTIONS
+    }
+}
